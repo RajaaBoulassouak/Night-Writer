975fd371db95bc1699d52fcb10151805a1680971
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-09 16:52:51 -0600

    update night_writer.rb & translation.rb/wrong input between the slice and translate methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,10 +2,13 @@ require './lib/translation'
 
     handle = File.open(ARGV[0], "r")
     incoming_text = handle.read
-    handle.close
-
+    handle.close  
+    
     translation = Translation.new
-    translate = translation.translate_text_to_braille(incoming_text)
+    sliced_text = translation.slice_text(incoming_text)
+    require "pry"; binding.pry
+    translate = translation.translate_text_to_braille(sliced_text)
+    # require "pry"; binding.pryin
 
     writer = File.open(ARGV[1], "w")
     writer.write(translate)
