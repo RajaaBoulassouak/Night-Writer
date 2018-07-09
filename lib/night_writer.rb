0cf4d492446c9358d3c0ecd6951680d15ba40b3e
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-09 12:57:56 -0600

    update night_writer.rb

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,9 +1,14 @@
-handle = File.open(ARGV[0], "r")
-incoming_text = handle.read
-handle.close
+require './lib/translation'
 
-writer =File.open(ARGV[1], "w")
-writer.write(incoming_text)
-writer.close
+    handle = File.open(ARGV[0], "r")
+    incoming_text = handle.read
+    handle.close
 
-puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
+    translation = Translation.new
+    translate = translation.translate_text_to_braille(incoming_text)
+
+    writer = File.open(ARGV[1], "w")
+    writer.write(translate)
+    writer.close
+
+    puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
