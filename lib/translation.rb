ff5fac51c78a9805e7bc982d17505799d425adcd
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-10 08:52:24 -0600

    WIP - added format_braille_output method; reverted to prior translate_text_to_braille method

--- lib/translation.rb
+++ lib/translation.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Translation
 attr_reader :english_to_braille
   def initialize
@@ -32,32 +34,43 @@ attr_reader :english_to_braille
     }
   end
   
-  def slice_text(text)
-  sliced_text = []
-  ((text.length / 80) +1).times do
-    sliced_text << text.slice!(0..79)
-    end
-  end
+  # def slice_text(incoming_text)
+  # sliced_text = []
+  # ((incoming_text.length / 80) +1).times do
+  #   sliced_text << incoming_text.slice!(0..79)
+  #   end
+  #   return sliced_text
+  #   binding.pry 
+  # end
+  # 
+  # def translate_text_to_braille(incoming_text)
+  #   passed_output = slice_text(incoming_text)
+  #   passed_output.chars.map do |character|
+  #     english_to_braille[character]
+  #   end 
+  # end 
   
+
   def translate_text_to_braille(string)
-    # require "pry"; binding.pry
-    
-      string.chars.map do |character|
-        english_to_braille[character]
+    string.chars.map do |character|
+      english_to_braille[character]
     end
     # braille_characters.transpose#.each_slice(80).with_index
   end
   
-
-  # def translate_text_to_braille(string)
-  #   string.chars.map do |character|
-  #     english_to_braille[character]
-  #   end
-  #   # braille_characters.transpose#.each_slice(80).with_index
-  # end
+  def format_braille_output(translated_array)
+    # binding.pry 
+    line_array = translated_array.transpose
+    line_one = line_array[0].join
+    line_two = line_array[1].join
+    line_three = line_array[2].join
+    "#{line_one[0, 79]}\n""#{line_two[0, 79]}\n""#{line_three[0, 79]}\n\n""#{line_one[80, 159]}\n""#{line_two[80, 159]}\n""#{line_three[80, 159]}\n\n""#{line_one[160, 239]}\n""#{line_two[160, 239]}\n""#{line_three[160, 239]}\n\n""#{line_one[240, 319]}\n""#{line_two[240, 319]}\n""#{line_three[240, 319]}"
+  end
 
 
 end
 
-# translation = Translation.new
-# p translation.translate_text_to_braille("wonderful")
+translation = Translation.new
+# p translation.translate_text_to_braille("xx")
+# p translation.translate_text_to_braille("xx").transpose
+p translation.format_braille_output([["00", "..", "00"], ["00", "..", "00"]])
