87f7e4e04777f992292e539e25e62fcf7db635c7
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-10 22:41:07 -0600

    updates iteration 2 completion

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,12 +2,11 @@ require './lib/translation'
 
     handle = File.open(ARGV[0], "r")
     incoming_text = handle.read
-    handle.close  
+    handle.close
     
     
     translation = Translation.new
     translated_text = translation.translate_text_to_braille(incoming_text)
-    
     braille_output = translation.format_braille_output(translated_text)
   
 
